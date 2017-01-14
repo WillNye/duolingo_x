923f38de89c60e0f8c945df4a13b360c1369f960
Will <willbeas88@gmail.com>
2017-01-13 21:01:05 -0600

    Fixed CSRF issue Made a simple quiz script

--- duolingo_x/settings.py
+++ duolingo_x/settings.py
@@ -128,10 +128,10 @@ STATIC_URL = '/static/'
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        # 'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
     ),
     'DEFAULT_PERMISSION_CLASSES': (
-        # 'rest_framework.permissions.IsAuthenticated',
+        'rest_framework.permissions.IsAuthenticated',
     )
 }
 
--- post_data.py
+++ post_data.py
@@ -1,11 +1,55 @@
 # -*- coding: latin-1 -*-
 
-from os import path
 import json
+from os import path
+from random import randint
 
 import requests
 
 CURRENT_URL = 'http://127.0.0.1:8000/{}'
+
+
+def nightly_warmup():
+    session = requests.Session()
+    response = session.get(CURRENT_URL.format('api-auth/login/'))
+    csrftoken = response.cookies['csrftoken']
+    session.post(CURRENT_URL.format('api-auth/login/'),
+                 data={'username': 'wbeasley', 'password': 'N0ahArthur'},
+                 headers={'X-CSRFToken': csrftoken},
+                 allow_redirects=False)
+    csrftoken = response.cookies['csrftoken']
+
+    language = 'italian'
+
+    response = session.get(CURRENT_URL.format('api-v1/phrases/'),
+                           headers={'X-CSRFToken': csrftoken})
+    phrases = response.json()
+
+    response = session.get(CURRENT_URL.format('api-v1/languages/'),
+                           data={'name': language},
+                           headers={'X-CSRFToken': csrftoken})
+    language = response.json()
+    audio_base = language[0]['audio_base']
+
+    while len(phrases) > 0:
+        cur_phrase_pos = randint(0, len(phrases) - 1)
+        cur_phrase = phrases[cur_phrase_pos]
+        is_listen = randint(0,1)
+        if is_listen == 0:
+            print("URL: {}{} \n\n".format(audio_base, cur_phrase['audio_id']))
+            input("Press any button to see the answer")
+            print("Phrase: {} \n\n".format(cur_phrase['english_translation']))
+        else:
+            print("Phrase: {} \n\n".format(cur_phrase['english_translation']))
+            input("Press any button to see the answer")
+            print("URL: {}{} \n\n".format(audio_base, cur_phrase['audio_id']))
+
+        was_correct = input("Were you correct? \n1=Yes and 2=No")
+        try:
+            if was_correct == int(1):
+                del(phrases[cur_phrase_pos])
+        except:
+            print("That's not an option dumbass")
 
 
 def read_sql():
@@ -17,7 +61,9 @@ def read_sql():
     response = session.post(CURRENT_URL.format('api-auth/login/'),
                            data={'username': 'wbeasley', 'password': 'N0ahArthur'},
                            headers={'X-CSRFToken': csrftoken})
-    response = session.get(CURRENT_URL.format('api-v1/phrases/'), params={'id': 23})
+    csrftoken = response.cookies['csrftoken']
+
+    response = session.get(CURRENT_URL.format('api-v1/phrases/'), params={'id': 23}, headers={'X-CSRFToken': csrftoken})
     test = response.json()
     print(test)
     print(response.url)
@@ -34,10 +80,22 @@ def update_sql():
 
     session.post(CURRENT_URL.format('api-auth/login/'),
                  data={'username': 'wbeasley', 'password': 'N0ahArthur'},
-                 headers={'X-CSRFToken': csrftoken})
+                 headers={'X-CSRFToken': csrftoken},
+                 allow_redirects=False)
 
     response = session.get(CURRENT_URL.format('api-v1/languages/'),
-                           data={'name': 'italian'})
+                           data={'name': 'italian'},
+                           allow_redirects=False)
+
+    response = session.get(CURRENT_URL.format('api-v1/phrases/'))
+
+    phrases = response.json()
+    for phrase in phrases:
+        session.delete(CURRENT_URL.format('api-v1/phrases/'),
+                       data={'id': phrase['id']},
+                       headers={'X-CSRFToken': csrftoken},
+                       allow_redirects=False)
+
     language = response.json()
     language = language[0]['id']
     for phrase in keys['translations']:
@@ -53,7 +111,6 @@ def update_sql():
 
 
 def add_phrase():
-    print(u"La torta � dolce")
     session = requests.Session()
     language = "Italian"
     english = "The cup"
@@ -62,6 +119,13 @@ def add_phrase():
     url = url.split("/")
     url_id = url[len(url)-1]
 
+    response = session.get(CURRENT_URL.format('api-auth/login/'))
+    csrftoken = response.cookies['csrftoken']
+
+    session.post(CURRENT_URL.format('api-auth/login/'),
+                 data={'username': 'wbeasley', 'password': 'N0ahArthur'},
+                 headers={'X-CSRFToken': csrftoken})
+
     response = session.get(CURRENT_URL.format('api-v1/languages/'),
                            data={'name': language})
     language = response.json()
@@ -69,14 +133,10 @@ def add_phrase():
                      data={'english_translation': english,
                            'foreign_translation': italian,
                            'audio_id': url_id,
-                           'language': language[0]['id']})
+                           'language': language[0]['id']},
+                     headers={'X-CSRFToken': csrftoken})
     print(r.text)
 
 
-def post_test():
-    word = u"è".encode('latin-1').decode('latin-1')
-    print(word)
-
-
 if __name__ == '__main__':
-    read_sql()
+    nightly_warmup()
